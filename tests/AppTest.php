9680a36b912896f7c4528becd6af11c39b04d552
heinzwalace <heinzwalace@gmail.com>
2024-03-18 11:02:08 +0100

    first

---
+++ tests/AppTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class AppTest extends TestCase {
+
+    public function testTestsAreWorking(){
+        $this->assertEquals(5, 1+1);
+    }
+
+
+
+
+}
